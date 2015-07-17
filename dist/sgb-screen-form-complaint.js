2b591ca7d5928642ada172b7e443765f98a7b6e6
luismiglietti <luismiglietti@gmail.com>
2015-07-17 16:16:39 -0430

    form val

--- dist/sgb-screen-form-complaint.js
+++ dist/sgb-screen-form-complaint.js
@@ -6,11 +6,24 @@ angular.module('sgb-screen-form-complaint', ['megazord'])
   .controller('sgb-screen-form-complaint-controller',
               ['_screen', '_screenParams','$stateParams', '$scope',
               function(_screen, _screenParams, $stateParams, $scope){
-   			  _screen.initialize($scope, _screenParams);
-   			  $scope.requests = _screenParams.requests;
-   			  $scope.recipients = _screenParams.recipients;
 
-  }]); 
+ _screen.initialize($scope, _screenParams);
+ $scope.requests = _screenParams.requests?_screenParams.requests:['NA'];
+ $scope.recipients = _screenParams.recipients?_screenParams.recipients:['NA'];
+ 
+ $scope.content = {
+  	type: $scope.requests[0],
+  	recipient: $scope.recipients[0],
+  	comment: ''
+ };
+
+ /* $scope.send = function() {
+  	$scope.content.type =
+  }*/
+
+}]); 
+
+
 
 },{}]},{},[1]);
 
