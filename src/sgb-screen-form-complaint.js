a96d614ba84897ab1325266b2f074b685b035b9b
luismiglietti <10-10459@usb.ve>
2015-07-16 23:01:11 -0430

    dist, .gitignore

--- src/sgb-screen-form-complaint.js
+++ src/sgb-screen-form-complaint.js
@@ -3,8 +3,8 @@
 angular.module('sgb-screen-form-complaint', ['megazord'])
 
   .controller('sgb-screen-form-complaint-controller',
-              ['_router', '_screen', '_screenParams','$stateParams', '$scope', 'lodash',
-              function(_router, _screen, _screenParams, $stateParams, $scope, _){
+              ['_screen', '_screenParams','$stateParams', '$scope',
+              function(_screen, _screenParams, $stateParams, $scope){
    			  _screen.initialize($scope, _screenParams);
 
   }]); 
