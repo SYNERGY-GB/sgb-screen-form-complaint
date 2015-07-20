a09bf20907a334c905e5ab90d57f62175e46d34e
luismiglietti <luismiglietti@gmail.com>
2015-07-20 14:29:28 -0430

    generalizacion pantalla

--- src/sgb-screen-form-complaint.js
+++ src/sgb-screen-form-complaint.js
@@ -2,19 +2,20 @@
 
 angular.module('sgb-screen-form-complaint', ['megazord'])
 
-  .controller('sgb-screen-form-complaint-controller',
+	.controller('sgb-screen-form-complaint-controller',
               ['_screen', '_screenParams','$stateParams', '$scope',
               function(_screen, _screenParams, $stateParams, $scope){
 
- _screen.initialize($scope, _screenParams);
- $scope.services = _screenParams.services?_screenParams.services:['NA'];
- $scope.types = _screenParams.types?_screenParams.types:['NA'];
- 
- $scope.content = {
-  	service: $scope.services[0],
-  	type: $scope.types[0],
-  	details: ''
- };
+	    _screen.initialize($scope, _screenParams);
+	    $scope.form_msg   = _screenParams.form_msg?_screenParams.form_msg:[]; 
+	    $scope.combobox_1 = _screenParams.combobox_1?_screenParams.combobox_1:['NA'];
+	    $scope.combobox_2 = _screenParams.combobox_2?_screenParams.combobox_2:['NA'];
+
+		$scope.content = {
+		 	selection_1: $scope.combobox_1[0],
+		 	selection_2: $scope.combobox_2[0],
+		 	input_text: ''
+		};
 
  /* $scope.send = function() {
   	$scope.content.type =
