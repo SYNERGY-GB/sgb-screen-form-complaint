d892e7537c1f032054581c2eb6f796eca695ee20
luismiglietti <luismiglietti@gmail.com>
2015-07-22 17:12:02 -0430

    fixed template

--- dist/sgb-screen-form-complaint.js
+++ dist/sgb-screen-form-complaint.js
@@ -7,12 +7,10 @@ angular.module('sgb-screen-form-complaint', ['megazord'])
               ['_screen', '_screenParams','$stateParams', '$scope',
               function(_screen, _screenParams, $stateParams, $scope){
 
-        
 	    _screen.initialize($scope, _screenParams);
 	    $scope.form = $stateParams.data;
-	    $scope.form.combobox2 = [];
-	    $scope.form.combobox_2 = $scope.form.combobox2?$scope.form.combobox2
-	    											  :(_screenParams.combobox2?_screenParams.combobox2
+	    $scope.form.combobox_2 = $scope.form.combobox2_?$scope.form.combobox_2
+	    											   :(_screenParams.combobox_2?_screenParams.combobox_2
 	    											  						   :['default_1','default_2']);
 		$scope.content = {
 		 	selection_1: $scope.form.combobox_1[0],
