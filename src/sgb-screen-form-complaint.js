5ad5094d7f1286c6facc082c65c34c09c30bb42b
luismiglietti <luismiglietti@gmail.com>
2015-07-22 14:52:56 -0430

    fixed params

--- src/sgb-screen-form-complaint.js
+++ src/sgb-screen-form-complaint.js
@@ -2,27 +2,29 @@
 
 angular.module('sgb-screen-form-complaint', ['megazord'])
 
+
 	.controller('sgb-screen-form-complaint-controller',
               ['_screen', '_screenParams','$stateParams', '$scope',
               function(_screen, _screenParams, $stateParams, $scope){
 
+        
 	    _screen.initialize($scope, _screenParams);
-	    $scope.form_msg   = _screenParams.form_msg?_screenParams.form_msg:false; 
-	    $scope.combobox_1 = _screenParams.combobox_1?_screenParams.combobox_1:false;
-	    $scope.combobox_2 = _screenParams.combobox_2?_screenParams.combobox_2:['default_1','default_2'];
-
+	    $scope.form = $stateParams.data;
+	    $scope.form.combobox2 = [];
+	    $scope.form.combobox_2 = $scope.form.combobox2?$scope.form.combobox2
+	    											  :(_screenParams.combobox2?_screenParams.combobox2
+	    											  						   :['default_1','default_2']);
 		$scope.content = {
-		 	selection_1: $scope.combobox_1[0],
-		 	selection_2: $scope.combobox_2[0],
+		 	selection_1: $scope.form.combobox_1[0],
+		 	selection_2: $scope.form.combobox_2[0],
 		 	input_text: ''
 		};
 
- /* $scope.send = function() {
-  	$scope.content.type =
-  }*/
-
-}]); 
+		/* $scope.send = function() {
+		  	$scope.content.type =
+		}*/
 
 
-}]); 
+	}]); 
+
 
