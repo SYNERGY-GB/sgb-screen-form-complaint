cf148013befba5658670c83a5c727a2b2f1083bb
luismiglietti <luismiglietti@gmail.com>
2015-07-29 12:58:50 -0430

    Change in controller

--- src/sgb-screen-form-complaint.js
+++ src/sgb-screen-form-complaint.js
@@ -10,19 +10,13 @@ angular.module('sgb-screen-form-complaint', ['megazord'])
         
 	    _screen.initialize($scope, _screenParams);
 	    $scope.form = $stateParams.data;
-	    $scope.form.combobox_2 = $scope.form.combobox2_?$scope.form.combobox_2
-	    											   :(_screenParams.combobox_2?_screenParams.combobox_2
-	    											  						   :['default_1','default_2']);
+	    $scope.form.combobox_2 = $scope.form.combobox2_?$scope.form.combobox_2:['default_1','default_2']);
+
 		$scope.content = {
 		 	selection_1: $scope.form.combobox_1[0],
 		 	selection_2: $scope.form.combobox_2[0],
 		 	input_text: ''
 		};
-
-		/* $scope.send = function() {
-		  	$scope.content.type =
-		}*/
-
 
 	}]); 
 
