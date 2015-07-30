cd13e71c94f8add228c914745fbc25d97f4a1c45
luismiglietti <luismiglietti@gmail.com>
2015-07-30 11:50:33 -0430

    renamed repository

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ gulp.task('build', function(){
         return b.bundle();
     });
 
-    return gulp.src('src/sgb-screen-schedule.js')
+    return gulp.src('src/sgb-screen-short-form.js')
         .pipe(browserified)
         .pipe(gulp.dest('./dist/'));
 });
