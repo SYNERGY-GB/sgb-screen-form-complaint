a7f26184ea3eba90a36b70bfb091c3bfaf4f208c
luismiglietti <luismiglietti@gmail.com>
2015-08-03 17:19:23 -0430

    fixed popup

--- src/sgb-screen-short-form.js
+++ src/sgb-screen-short-form.js
@@ -11,26 +11,25 @@ angular.module('sgb-screen-short-form', ['megazord'])
 	    $scope.form = $stateParams.data;
 	    $scope.form.combobox_2 = $scope.form.combobox2_?$scope.form.combobox_2:['default_1','default_2'];
 
-		$scope.content = {
-		 	selection_1: $scope.form.combobox_1[0],
-		 	selection_2: $scope.form.combobox_2[0],
-		 	input_text: ''
+		$scope.resetContent = function() {
+			$scope.content = {
+			 	selection_1: $scope.form.combobox_1[0],
+			 	selection_2: $scope.form.combobox_2[0],
+			 	input_text: ''
+			}
 		};
-
+		$scope.resetContent(); 
 
 		$scope.send = function() {
-
 			$translate('popup_msg').then(function(msg) {
-      			$scope.popupMsg = msg;
+				var alertPopup = $ionicPopup.alert({
+		    		title: msg,
+  				});
+  				alertPopup.then(function(res) {
+     				$scope.resetContent();
+   				});
 			});
-		   	var alertPopup = $ionicPopup.alert({
-		    	title: $scope.popupMsg,
-  			});
-   			alertPopup.then(function(res) {
-     			console.log('Thank you for not eating my delicious ice cream cone');
-   			});
  		};
 
 	}]); 
-
 
